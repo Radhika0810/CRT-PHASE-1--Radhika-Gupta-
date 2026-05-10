a8c23d378da56c660e5e20f39543dd20fe41c0c8
Radhika <radhikagupta.100805@gmail.com>
2026-05-10 09:12:22 +0530

    normal

---
+++ Day 9/9b.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[2][5];
+  for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
